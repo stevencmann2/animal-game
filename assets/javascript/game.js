fc08875c7fb99b0c302806404abd9fa643401b56
Steven Mann <stevencmann2@gmail.com>
2019-10-11 11:50:59 -0500

    added correct JQuery version; created click events

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+//This is my JavaScript file linked with my Animal Game//
+
+// Declaring Variables//
+let wins= 0;
+let losses = 0;
+
+
+// Declaring on click events the icons //
+$(".horse").on("click", function() {
+    alert("you clicked the horse");
+  });
+
+  $(".dog").on("click", function() {
+    alert("you clicked the dog");
+  });
+
+  $(".bird").on("click", function() {
+    alert("you clicked the bird");
+  });
+
+  $(".frog").on("click", function() {
+    alert("you clicked the frog");
+  });
+
+
+
+
+
