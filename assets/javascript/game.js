e89587633ec6caa1acd11d49c365861632900998
Steven Mann <stevencmann2@gmail.com>
2019-10-11 13:10:01 -0500

    changed random #generator into a let so it would print to doc easier

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,23 @@
 // Declaring Variables//
 let wins= 0;
 let losses = 0;
+let playerScore = 0;
+// const for random target # generator
 
-// this is a funciton for the target score value genreator //
-function targetScoreMaker(min, max) {
-    min = Math.ceil(19);
-    max = Math.floor(121);
-    return Math.floor(Math.random() * (121 - 19)) + 19; //The maximum is exclusive and the minimum is inclusive
-  }
 
-  console.log(targetScoreMaker());
+// this is a variable for the target score value generator //
+let targetScore = Math.floor(Math.random() * (121 - 19)) + 19; //The maximum is exclusive and the minimum is inclusive
+console.log(targetScore);
 
+// creating an asignment to show the target score in the document //
+$(".targetscore").text(targetScore);
+
+// creating an assigmnment to show the player score at the moment//
+$(".playerscore").text(playerScore);
+
+// putting values in the place of wins & losses for the moment
+$(".wins").text(wins);
+$(".losses").text(losses);
 
 // Declaring on click events for the icons //
 $(".horse").on("click", function() {
@@ -31,6 +38,7 @@ $(".horse").on("click", function() {
     alert("you clicked the frog");
   });
 
+  
 
 
 
