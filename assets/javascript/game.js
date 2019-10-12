c5aa77c895574feecf46e385990d526f184625b4
Steven Mann <stevencmann2@gmail.com>
2019-10-12 10:00:43 -0500

    created the variable to sum current score; general cleaning of code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,6 @@ let wins = 0;
 let losses = 0;
 let playerScore = 0;
 
-
-
-
 // this is a variable for the target score value generator //
 let targetScore = Math.floor(Math.random() * (121 - 19)) + 19; //The maximum is exclusive and the minimum is inclusive
 console.log(targetScore);
@@ -20,8 +17,6 @@ let frog = Math.floor(Math.random() * (13 - 1)) + 1;
 
 console.log (horse, dog, bird, frog);
 
-
-
 // putting values in the place of wins & losses for the moment
 $(".wins").text(wins);
 $(".losses").text(losses);
@@ -32,72 +27,58 @@ $(".playerscore").text(playerScore);
 // creating an asignment to show the target score in the document //
 $(".targetscore").text(targetScore);
 
-//assigning the animal variables to icons with an onclick event/ prints to webpage//
-/*
-$(".horse").on("click", function() {
-    $(".playerscore").text(horse);
-});
-*/
 
-  $(".dog").on("click", function() {
-    $(".playerscore").text(dog);
-});
-  $(".bird").on("click", function() {
-    $(".playerscore").text(bird);
-});
-  $(".frog").on("click", function() {
-    $(".playerscore").text(frog);
 
-});
-console.log(typeof playerScore);
-/*
-// making a sum function of the icon values//
-function sumNumbers(playerScore) {
-    let playerSum = 0;
-    for (let i = 0; i<=playerScore.length ; i++) {
-        playerSum = playerScore + i;
-    }
-    return playerSum;  
-}
-
-console.log(typeof playerScore);
-console.log(playerScore);
-*/
 // Loop where the the score increase 
+//platerSum will be a new variable defining the current sum after a click event 
+let playerSum = 0;
+
+
+//assigning the animal variables to icons with an onclick event//
+//also sums the click event into the total (playerSum)//
+//prints the new value to player score //
 
 $(".horse").on("click", function() {
-    let playerSum = playerScore += horse;
+    playerSum = playerScore += horse;
     $(".playerscore").text(playerSum);
-    // need to make a new total to keep track of function? new variable? 
-   // playerScore.push(horse); //pushes horse into playerScore object, now part of objectplayerScore 
-  // $(".playerscore").text(sumNumbers(playerScore)); // prints function to the document 
 });
 
 
 $(".dog").on("click", function() {
-    let playerSum = playerScore += dog;
+    playerSum = playerScore += dog;
     $(".playerscore").text(playerSum);
 });
 
 $(".bird").on("click", function() {
-    let playerSum = playerScore += bird;
+    playerSum = playerScore += bird;
     $(".playerscore").text(playerSum);
 });
 
 $(".frog").on("click", function() {
-    let playerSum = playerScore += frog;
+    playerSum = playerScore += frog;
     $(".playerscore").text(playerSum);
 });
 
+console.log(typeof playerSum);
 
 // condition for win/loss//
+// SCOPE ISSUE! ONLY CHECKING AT THE START OF THE RUN, make into a function and check whenever clicked
+// or make a loop 
+//use a prompt to restart? by setting value to zero 
 if ((playerSum === targetScore)){
     wins++;
     alert("did this work, you win");
-} else ((playerSum > targetScore)) {
+} 
+
+
+/*
+
+else ((playerSum > targetScore)) {
     losses++;
     alert("you lost the game");
 )};
+
+*/
 
 
 
